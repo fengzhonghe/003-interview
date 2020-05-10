0bf1d243d1812c4a3e55742286a2ab67125515f7
li2 <646801716@qq.com>
2020-05-10 11:00:18 +0800

    second_branch's function add and commit .

---
+++ my-arrary/src/com/sy/myarray/NoConfirmFunction_Branch.java
@@ -0,0 +1,14 @@
+package com.sy.myarray;
+
+public class NoConfirmFunction_Branch {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void noConfirmFunction () {
+        //...
+        System.out.println("...");
+    }
+}
