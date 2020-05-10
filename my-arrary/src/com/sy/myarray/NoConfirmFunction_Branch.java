f7fafef58c6c5e7581f5118401e68dd70de25664
fengzhonghe <sylwl0935@163.com>
2020-05-10 13:11:04 +0800

    make conflict by fengzhonghe .

--- my-arrary/src/com/sy/myarray/NoConfirmFunction_Branch.java
+++ my-arrary/src/com/sy/myarray/NoConfirmFunction_Branch.java
@@ -3,8 +3,8 @@ package com.sy.myarray;
 public class NoConfirmFunction_Branch {
 
     public static void main(String[] args) {
-        noConfirmFunction();
-    }
+        noConfirmFunction();//make conflict by fengzhonghe .
+}
 
 
     public static void noConfirmFunction() {
