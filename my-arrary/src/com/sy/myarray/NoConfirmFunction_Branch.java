e3c7149b14b06b8673ea7ed3a61bdc3c854e8a4b
fengzhonghe <sylwl0935@163.com>
2020-05-10 11:36:08 +0800

    second _branch update .

--- my-arrary/src/com/sy/myarray/NoConfirmFunction_Branch.java
+++ my-arrary/src/com/sy/myarray/NoConfirmFunction_Branch.java
@@ -3,11 +3,11 @@ package com.sy.myarray;
 public class NoConfirmFunction_Branch {
 
     public static void main(String[] args) {
-
+        noConfirmFunction();
     }
 
 
-    public static void noConfirmFunction () {
+    public static void noConfirmFunction() {
         //...
         System.out.println("...");
     }
